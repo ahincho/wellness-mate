a03c3cab17ae5b5bc5f2be340ee57f6ee08c86ec
ahincho <ahincho@unsa.edu.pe>
2025-04-13 22:09:28 -0500

    Refactor: Remove NestJs Graph

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory, Reflector } from '@nestjs/core';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { ConfigService } from '@nestjs/config';
 import { JwtAuthGuard } from '@auth/guards/jwt.auth.guard';
-import { SpelunkerModule } from 'nestjs-spelunker';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -26,27 +24,6 @@ async function bootstrap() {
   };
   app.enableCors(corsOptions);
   const port = configService.get<number>('APP_PORT') || 3000;
-  if (configService.get<string>('APP_ENVIRONMENT') === 'development') {
-    void generateDependencyGraph(app);
-  }
   await app.listen(port);
 }
 bootstrap();
-
-async function generateDependencyGraph(app: INestApplication) {
-  const tree = SpelunkerModule.explore(app);
-  const root = SpelunkerModule.graph(tree);
-  const edges = SpelunkerModule.findGraphEdges(root);
-  const mermaidEdges = edges
-    .map(({ from, to }) => `  ${from.module.name}-->${to.module.name}`)
-    .filter(
-      (edge) =>
-        !edge.includes('FilteredModule') && !edge.includes('OtherExample'),
-    )
-    .sort();
-  fs.writeFileSync(
-    'deps.mermaid',
-    `graph LR
-      ${mermaidEdges.join('\n')}`,
-  );
-}
